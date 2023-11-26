eef84f457e5a47d7fa6be859e1e1170841335ea8
droid-anuj <121409495+droid-anuj@users.noreply.github.com>
2023-11-26 21:20:36 +0530

    Update HomePage.js

--- frontend/src/Components/HomePage.js
+++ frontend/src/Components/HomePage.js
@@ -18,7 +18,7 @@ function HomePage() {
   const fetchContactData = () => {
     axios
 
-      .get("https://emptycup-backend.vercel.app/api/contacts")
+      .get("https://emptycup-zeta.vercel.app/api/contacts")
 
       .then((response) => {
         const data = response.data;
