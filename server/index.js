d6c72d59832280cd5e204ac4619bcd6022de9b0c
droid-anuj <121409495+droid-anuj@users.noreply.github.com>
2023-11-26 19:46:47 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,12 @@ const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+	{
+		origin:"emptycupserver.vercel.app",
+	}
+)
+));
 app.use("/api", apiRoutes);
 
 
